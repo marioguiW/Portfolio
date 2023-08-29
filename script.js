15aa59b5a4bdde8b71ed9ce5b01c7993aa34fe5f
Mario Guilherme Wilhelms <marioguilhermew@gmail.com>
2023-08-29 12:38:20 -0300

    Adicionando novo projeto - calculadora

--- script.js
+++ script.js
@@ -4,29 +4,34 @@ ScrollReveal().reveal('.footer-conteudo', {delay: 300});
 
 const project = [
     {
-        nome: "Monibank-Forms-Validation",
+        nome: "Monibank Forms Validation",
         url: "https://marioguiw.github.io/ValidacaoDeFormulario-JS/pages/abrir-conta-form.html",
         img: "./img/Screenshot_Monibank.jpg"
     },
     {
-        nome: "Crud-Bag",
+        nome: "Crud Bag",
         url: "https://marioguiw.github.io/Mochila-Viagem-JS/",
         img: "./img/Screenshot_Crud_Bag.jpg"
     },
     {
-        nome: "Netflix-Clone",
+        nome: "Netflix Clone",
         url: "https://mario-flix-js.vercel.app/",
         img: "./img/Screenshot_Netflix-Clone.jpg"
     },
     {
-        nome: "Loja-Livros",
+        nome: "Loja Livros",
         url: "https://loja-livros-blond.vercel.app/",
         img: "./img/Screenshot_Loja_Livros.jpg"
     },
     {
-        nome: "Acerta-Numero",
+        nome: "Acerta Numero",
         url: "https://acerta-numeros-js.vercel.app/",
         img: "./img/Screenshot_Acerta_Numero.jpg"
+    },
+    {
+        nome: "Calculadora",
+        url: "https://calculadora-js-peach.vercel.app/",
+        img: "./img/Screenshot_Calculadora.jpeg"
     }
 ]
 
